b01dad36de8b10486ff1178d732433b0a07a8ba0
kawing <o2o.thomasli@gmail.com>
2018-12-20 15:43:04 +0800

    add static php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Test Title</title>
+</head>
+<body>
+
+<h1>This is a static testing page</h1>
+<p>This page is used for testing static html page display.</p>
+
+</body>
+</html>
